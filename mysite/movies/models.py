90c0d2db5d0d2c69c0efa82bb67a28adc6cc3fa9
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-10-29 12:50:57 -0500

    made migrations

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class MovieData (models.Model):
+    name = models.CharField(max_length=200)
+    director = models.CharField(max_length = 200)
+    duration = models.FloatField()
+    rating = models.FloatField()
